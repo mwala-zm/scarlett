bd7624c17c8cd784029ce70e954e4dd77ccb0f86
mwala-zm <mwalamulenga.zt@gmail.com>
2023-04-04 05:59:24 +0200

    Configured assets

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    # Get all users
+    field :users, [Types::UserType], null: false
+    def users
+      User.all
+    end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # Get a specific user
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+    def user(id:)
+      User.find(id)
     end
   end
 end
