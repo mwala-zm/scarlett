dcac78ea7dea76544d632deebb990a53382e19fb
mwala-zm <mwalamulenga.zt@gmail.com>
2023-04-04 22:47:25 +0200

    added devise to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # make user ID UUID
   before_create do
     self.id = SecureRandom.uuid
