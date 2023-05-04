29de1a8338f874e210a1b00c1e520f7a727848cb
mwala-zm <mwalamulenga.zt@gmail.com>
2023-05-04 19:23:18 +0200

    add mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
+		field :deleteUser, mutation: Mutations::DeleteUser
+		field :createUser, mutation: Mutations::CreateUser
+		field :updateUser, mutation: Mutations::UpdateUser
     field_class GraphqlDevise::Types::BaseField
   end
 end
