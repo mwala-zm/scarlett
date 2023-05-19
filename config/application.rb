037b6caba9c06a7b4050ee3ad2b84927f3312956
mwala-zm <mwalamulenga.zt@gmail.com>
2023-05-19 13:39:05 +0200

    fix: remove credentials from cors

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Scarlett
     config.api_only = true
 
     config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use config.session_store, config.session_options
+    config.middleware.use ActionDispatch::Cookies    
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     config.middleware.use ActionDispatch::Flash
     config.middleware.use Rack::MethodOverride
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, key: "_scarlett", domain: ""
 else
-Rails.application.config.session_store :cookie_store, key: "_scarlett", expire_after: 2.weeks
+Rails.application.config.session_store :cookie_store, key: "_scarlett"
 end
