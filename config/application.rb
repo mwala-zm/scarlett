4fb3333cace9afe4d8bb50f9c5e445c7c8e6a758
mwala-zm <mwalamulenga.zt@gmail.com>
2023-09-07 10:27:52 +0200

    hotfix

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Scarlett
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.factory_bot.definition_file_paths = ["custom/factories"]
+    #config.factory_bot.definition_file_paths = ["custom/factories"]
     config.api_only = true
 
     config.session_store :cookie_store, key: '_interslice_session'
