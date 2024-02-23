235db58d64a8b680ed50262b2b6c4d209017a49f
mwala-zm <mwalamulenga.zt@gmail.com>
2024-02-23 21:24:54 +0200

    Update mutation type format

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,13 +1,13 @@
 module Types
   class MutationType < Types::BaseObject
-		field :deleteField, mutation: Mutations::DeleteField
-		field :createField, mutation: Mutations::CreateField
-		field :updateField, mutation: Mutations::UpdateField
+    field :deleteField, mutation: Mutations::DeleteField
+    field :createField, mutation: Mutations::CreateField
+    field :updateField, mutation: Mutations::UpdateField
     field :deleteCrop, mutation: Mutations::DeleteCrop
-		field :createCrop, mutation: Mutations::CreateCrop
-		field :updateCrop, mutation: Mutations::UpdateCrop
-		field :deleteUser, mutation: Mutations::DeleteUser
-		field :updateUser, mutation: Mutations::UpdateUser
+    field :createCrop, mutation: Mutations::CreateCrop
+    field :updateCrop, mutation: Mutations::UpdateCrop
+    field :deleteUser, mutation: Mutations::DeleteUser
+    field :updateUser, mutation: Mutations::UpdateUser
     field_class GraphqlDevise::Types::BaseField
   end
 end
